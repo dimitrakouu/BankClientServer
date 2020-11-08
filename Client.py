b3f09996e192a6e98b6bb25efb9f96173d8e91b3
dimitrakouu <dimitraa.kk@gmail.com>
2020-11-08 23:17:32 +0200

    Established connection and developed first step of validation

--- Client.py
+++ Client.py
@@ -1,27 +1,43 @@
 import socket
 
+class Client():
+    def __init__(self):
+        self.client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_host = '127.0.0.1'#socket.gethostname()
+        self.server_port = 1234 # socket server port number
 
-def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    def connection_with_server(self):
+        print('Waiting for connection')
+        try:
+           connection= (self.server_host, self.server_port)
+        except socket.error as e:
+            print(str(e))
+        self.client_socket.connect(connection)
+        print('Connection established.')
+        users_credentials=0
 
-    client_socket = socket.socket()  # instantiate
-    print('Waiting for connection')
-    try:
-        client_socket.connect((host, port))
-    except socket.error as e:
-        print(str(e))
+     #   Response = self.client_socket.recv(1024)
+        while True:
+           if users_credentials == 0:
+                name = input("Give name: ")
+                self.client_socket.sendall(name.encode('utf-8'))
+                if name=='bye':
+                    break
+                response_from_server = self.client_socket.recv(1024).decode('utf-8')
+                print(response_from_server)
+           users_credentials += 1
 
+           request = input('Choose an option.\n1)Deposit.\n2)Cash-back.\n3)Exit\n')
+           if 'Exit' in request:
+               print('Exiting.')
+               self.client_socket.sendall('{}'.format('Exit ').encode('utf-8'))
+               self.client_socket.close()
+               break
+           self.client_socket.sendall(request.encode('utf-8'))
 
-    Response = client_socket.recv(1024)
-    while True:
-        Input = input('Say Something: ')
-        client_socket.send(str.encode(Input))
-        Response = client_socket.recv(1024)
-        print(Response.decode('utf-8'))
-
-    client_socket.close()  # close the connection
+        self.client_socket.close()  # close the connection
 
 
 if __name__ == '__main__':
-    client_program()
+    client=Client()
+    client.connection_with_server()
--- Server.py
+++ Server.py
@@ -1,46 +1,75 @@
 import socket
-from _thread import *
+import threading
 
 
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
-    ThreadCount=0
+class Server(object):
+    def __init__(self):
+        #create lock
+        self.lock=threading.Lock()
+        # get the hostname
+        self.host = '127.0.0.1' #socket.gethostname()
+        self.port = 1234  # initiate port no above 1024
+        print ("[+] New server socket thread started for " + self.host + ":" + str(self.port) )
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    try:
-        server_socket.bind((host, port))
-    except socket.error as e:
-        print(str(e))
 
-  #  server_socket.bind((host, port))  # bind host address and port together
-    print('Waitiing for a Connection..')
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
+    def start_server(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.bind((self.host, self.port))
+        print('Waitiing for a Connection..')
+        # configure how many client the server can listen simultaneously
+        server_socket.listen(5)
+        threads=[]
 
-    def threaded_client(connection):
-        connection.send(str.encode('Welcome to the Server\n'))
         while True:
-            data = connection.recv(2048)
-            server_message= input("Server says")
-            reply = 'Server Says: ' + server_message
-            if not data:
+             #accept client
+             connection, address = server_socket.accept()
+             print('Connected to: ' + address[0] + ':' + str(address[1]))
+             newthread= threading.Thread(target=self.threaded_client, args=(connection,)) # start_new_thread(threaded_client, (conn,))
+             newthread.start()
+         #    print('Thread Number: ' + threading.current_thread().name())
+          #   threads.append(newthread)
+        server_socket.close()
+
+
+        # for i in threads:
+        #     i.join()
+
+
+    def threaded_client(self, connection_thread):
+        name_option = connection_thread.recv(2048).decode('utf-8')
+        connection_thread.sendall(str.encode('Welcome to the Server ' + name_option))
+        while True:
+            #connection_thread.sendall(str.encode('Welcome '+ name_option))
+            option = connection_thread.recv(2048).decode('utf-8')
+            print('Client  option: ' + option)
+            if 'EXIT' in option.upper():
+                print('Session terminated.')
                 break
-            connection.sendall(str.encode(reply))
-        connection.close()
+            elif 'VALIDATION' in option.upper():
+                print('Validation')
+                pass
+            elif 'BALANCE' in option.upper():
+                print('Balance')
+                pass
+            elif 'CASH_BACK' in option.upper():
+                print('Cash back')
+                pass
+            else:
+                print('sth went wrong.')
+              #  connection_thread.sendall('sth went wrong'.encode('utf-8'))
+                connection_thread.sendall(str.encode('Sth went wrong\n'))
 
-    while True:
-        conn, address = server_socket.accept()
-        print('Connected to: ' + address[0] + ':' + str(address[1]))
-        start_new_thread(threaded_client, (conn,))
-        ThreadCount += 1
-        print('Thread Number: ' + str(ThreadCount))
-    server_socket.close()
+    def validate_user(self, user, password):
+        pass
 
+    def get_account(self, amount, user_id):
+        pass
 
+    def update_account(self, amount, user_id):
+        pass
 
 
 if __name__ == '__main__':
-    server_program()
+    server = Server()
+    server.start_server()
